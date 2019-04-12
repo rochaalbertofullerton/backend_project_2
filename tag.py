dca170bb1c05cd4e390ac953f86258efe9179046
Alberto Rocha <berto323@csu.fullerton.edu>
2019-04-11 19:55:54 -0700

    update 4/11 @ 7:55

--- tag.py
+++ tag.py
@@ -1,29 +1,8 @@
-import flask, sqlite3, hashlib
+import flask, sqlite3, hashlib, requests
 from datetime import datetime, date
 from flask import request , jsonify
-from flask_basicauth import BasicAuth 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
-
-
-class auth_User(BasicAuth):
-    def check_credentials(self, username, password):
-        conn = sqlite3.connect("ZL1API.db")
-        x = conn.cursor()
-        x.execute("SELECT password FROM user WHERE email=?",(username, ))
-        value = x.fetchone()
-
-        if value != None:
-            hashedpassword = hashlib.md5(password.encode())
-            if hashedpassword.hexdigest() == value[0]:
-                return True
-            else:
-                return False
-        else:
-            return False 
-                
-basic_auth = auth_User(app)
 
 #ADD TAGS FOR NEW URL  & ADD TAGS TO AN EXISTING URL
 #The json must contain "id", "tag"
@@ -31,45 +10,41 @@ basic_auth = auth_User(app)
 #The author must be created before before an article you can post a tag
 #If an error occurs it will reply with the error and 204 Not acceptable
 @app.route('/tag/<path:article>', methods=['POST'])
-@basic_auth.required
 def postTag(article):
-    conn = sqlite3.connect("ZL1API.db")
+    conn = sqlite3.connect("tags.db")
     x = conn.cursor()
     data = request.get_json()
-    keyid = data["id"]
     keytag = data["tag"]
     keyurl ='/article/' + article
-    x.execute("SELECT * FROM articles WHERE url =?", (keyurl,))
-    value = x.fetchone()
-    if value != None:
+    req = requests.get('http://localhost:5000'+keyurl)
+
+    if req.status_code == 200:
         try:
-            x.execute('INSERT INTO tag (id, tag, url) Values(?,?,?) ',(keyid,keytag, keyurl,))
+            x.execute('INSERT INTO tags (tags_content, tags_articles_url) Values(?,?) ',(keytag, keyurl,))
             conn.commit()
             x.close()
-            return jsonify("CREATED") , 201
+            return jsonify('CREATED') , 201
         except Exception as er:
             x.close()
             return str(er)
     else:
         x.close()
-        return jsonify("Article not found"), 204
+        return jsonify('ARTICLE NOT FOUND'), 404
     
 #REMOVE ONE OR MORE TAGS FROM AN INDIVIDUAL URL
 #The json must contain  "tag"
 #Example ----> /tag/The Road Not Taken1 {Hit Enter}
 #If an error occurs it will reply with the error and 204 Not acceptable
 @app.route('/tag/<path:article>', methods=['DELETE'])
-@basic_auth.required
 def deleteArticle(article):
-    conn = sqlite3.connect("ZL1API.db")
+    conn = sqlite3.connect("tags.db")
     x = conn.cursor()
     data = request.get_json()
     key = data["tag"]
     keyurl ='/article/' + article
-    x.execute('SELECT * FROM articles WHERE url=?' , (keyurl,))
-    value = x.fetchone()
+    req = requests.get('http://localhost:5000'+keyurl)
     
-    if value != None:
+    if req.status_code == 200:
         x.execute('SELECT * FROM tag WHERE url=? AND tag=?' , (keyurl,key,))
         valuetag = x.fetchone()
         if valuetag != None:
@@ -87,11 +62,11 @@ def deleteArticle(article):
 
 #RETRIEVE THE A LIST URLS WITH A GIVEN TAG
 #Example ----> /tag/The Road Not Taken1 {Hit Enter}
-@app.route("/tag/<path:tag>", methods=['GET'])
+@app.route("/tag/url/<path:tag>", methods=['GET'])
 def getarticleswithTag(tag):
-    conn = sqlite3.connect("ZL1API.db")
+    conn = sqlite3.connect("tags.db")
     x = conn.cursor()
-    x.execute('SELECT url  FROM tag WHERE tag=?', (tag,))
+    x.execute('SELECT tags_articles_url  FROM tags WHERE tags_content=?', (tag,))
     value = x.fetchall()
     if value != []:
         x.close()
@@ -102,12 +77,12 @@ def getarticleswithTag(tag):
 
 #RETRIEVE THE TAGS FOR AN INDIVIDUAL URL
 #Example ----> /tag/The Road Not Taken1 {Hit Enter}
-@app.route('/tag/url/<path:article>', methods=['GET'])
+@app.route('/tag/<path:article>', methods=['GET'])
 def gettagforUrl(article):
-    conn = sqlite3.connect("ZL1API.db")
+    conn = sqlite3.connect("tags.db")
     x = conn.cursor()
     key = '/article/' + article
-    x.execute('SELECT tag FROM tag WHERE url=?', (key,))
+    x.execute('SELECT tags_content FROM tags WHERE tags_articles_url=?', (key,))
     value = x.fetchall()
     if value != []:
         x.close()
